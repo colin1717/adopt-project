d70cc6cd85839fbcec76ca11df496317f53b4408
Colin Hancock <colin1717@mac.com>
2016-04-29 11:35:52 -0500

    linked db

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var mongoose = require('mongoose');
+mongoose.connect(process.env.DB_CONN_CATDOG);
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
