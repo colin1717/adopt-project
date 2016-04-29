fbf8d60a457ef8e68a26367e706a5c83f1925488
Colin Hancock <colin1717@mac.com>
2016-04-29 12:26:07 -0500

    backend for auth complete

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
+var passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'CatDog' });
 });
+
+router.post('/signup', function(req, res, next) {
+  var user = new User({ username: req.body.username });
+  User.register(user, req.body.password, function(error) {
+    if (error) {
+      res.send(error);
+    } else {
+      req.login(user, function(loginError) {
+        if (loginError) {
+          res.send(loginError)
+        } else {
+          res.redirect('/pets')
+        }
+      });
+    }
+  });
+});
+
+function checkLoggedIn(req, res, next){
+  if (req.isAuthenticated()){
+    next();
+  } else {
+    res.redirect('/');
+  }
+}
+
+router.get('/pets', checkLoggedIn, function(req, res, next){
+  res.send('/pets');
+})
 
 module.exports = router;
