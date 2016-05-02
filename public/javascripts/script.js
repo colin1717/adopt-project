dce8132ea466042a74417ce2c3a5ff64ca51c573
Haydeé Muñoz <haydeemunozdelarocha@gmail.com>
2016-05-02 11:01:25 -0500

    images populating, api working

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -29,14 +29,18 @@
 
 
 
-var thing = {"apikey":"***","objectType":"animals","objectAction":"publicSearch","search":{"calcFoundRows":"Yes","resultStart":0,"resultLimit":0,"fields":["animalName"],"filters":[{"fieldName":"animalStatus","operation":"equals","criteria":"Adopted"},{"fieldName":"animalOrgID","operation":"equals","criteria":"****"}]}};
-var encoded = JSON.stringify(thing);
+// var thing = {"apikey":"R0vKnZzZ","objectType":"animals","objectAction":"publicSearch","search":{"calcFoundRows":"Yes","resultStart":0,"resultLimit":0,"fields":["animalName"],"filters":[{"fieldName":"animalStatus","operation":"equals","criteria":"Adopted"},{"fieldName":"animalOrgID","operation":"equals","criteria":"Dog"}]}};
+// var encoded = JSON.stringify(thing);
 
 $.ajax({
-  url: "https://api.rescuegroups.org/http/json/?data=" + encoded,
+  url: "http://api.petfinder.com/pet.getRandom?key=7fe69d8a1ef29360d4fcf36d90a09254f554a394&location=78704&format=json&output=full" ,
   dataType: "jsonp",
   success: function(data) {
-        console.log(data.foundRows);
+        console.log(data.petfinder.pet);
+         $('.dog').html(
+            '<img src="' + data.petfinder.pet.media.photos.photo[1].$t + '"/>' +
+            '<div class="dog-name">' + data.petfinder.pet.name.$t + "<br> " + data.petfinder.pet.age.$t + ' years </div>'
+          );
   },
   error: function(xhr, status, error) {
     console.log('error');
