0cf1217e9acae1d1f350140590db95aa2154a43e
Jeanne Guinn <jeannesguinn@gmail.com>
2016-05-02 14:36:27 -0500

    update script.js to pull JSON from API. This commit works

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -10,9 +10,10 @@ $(document).ready(function(){
         success : function(data) {
             // stores result
             var result = '';
-            //
+            // set a variable of petfinder equal to the data passed into the success function
             var petfinder = data.petfinder;
 
+//-----------All of the following in the console.log() get the proper data for the pets from the API-----------------------
             console.log(petfinder.pet.name['$t']);
             console.log(petfinder.pet.animal['$t']);
             console.log(petfinder.pet.sex['$t']);
@@ -20,31 +21,21 @@ $(document).ready(function(){
             console.log(petfinder.pet.description['$t']);
             console.log(petfinder.pet.id['$t']);
 
+//---------Sets the p with the id of petNameInfo's html equal to the pet name-----------------------------------------------
+             $('#petNameInfo').html(petfinder.pet.name['$t']);
+
+
+/*The petImageHolder, gets the pet image in the object[0] of the array, petImageURL, takes
+the value of the url stored in the text of petImageHolder and places it into a url. Then, I've declared a variable of favorite and set it equal to the div with the class of dog. From
+there, we're doing a find an image in the favorite class and set the source attribute equal to  the url we've placed in the petImageURL varialbe*/
             var petImageHolder = petfinder.pet.media.photos.photo[0];
             var petImageURL = (petImageHolder['$t']);
             var favorite = $('.dog');
-
             favorite.find('img').attr('src', petImageURL);
 
-            $('#petNameInfo').html(petfinder.pet.name['$t']);
+//-------------------------END OF IMAGE PLACEMENT-----------------------------------------------------------------------
 
 
-            var infoHTML = '<ul>';
-            infoHTML += '<li>';
-            infoHTML += '<strong>Description</strong><br>';
-            infoHTML += petfinder.pet.description['$t'];
-            infoHTML += petfinder.pet.animal['$t'];
-            infoHTML += '</li>';
-
-            infoHTML += '</li>';
-
-            infoHTML += '</ul>';
-            // return infoHTML;
-            $('#petfinderInfo').html(infoHTML);
-
-            // $('#petfinderInfo').html(petfinder.pet.description['$t']);
-            //
-            console.log(petfinder);
         },
         error : function(request,error)
         {
