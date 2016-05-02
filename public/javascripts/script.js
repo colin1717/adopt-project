0b8e050e4620e9b5b3958ca0de9e887feec0e878
Jeanne Guinn <jeannesguinn@gmail.com>
2016-05-02 12:22:27 -0500

    getting animals in ajax call

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,75 +1,53 @@
 $(document).ready(function(){
-  var dogId;
 
-  function getNextDog() {
-    $.ajax({
-      url: '/pets',
-      method: 'GET',
-      dataType: 'json',
-      success: function(dogData) {
-        if (dogData.status) {
-          $('.dog').html('');
-          alert(dogData.status);
-        } else {
-          dogId = dogData._id;
+ $("#testButton").click(function(e) {
+  var url = 'http://api.petfinder.com/pet.getRandom?key=7fe69d8a1ef29360d4fcf36d90a09254f554a394&shelterid=KY305&output=full&format=json';
+  $.ajax({
+        type : 'GET',
+        data : {},
+        url : url+'&callback=?' ,
+        dataType: 'jsonp',
+        success : function(data) {
+            // stores result
+            var result = '';
+            //
+            var petfinder = data.petfinder;
 
-          $('.dog').html(
-            '<img src="' + dogData.photo + '"/>' +
-            '<div class="dog-name">' + dogData.name + "<br> " + dogData.age + ' years </div>'
-          );
+            console.log(petfinder.pet.name['$t']);
+            console.log(petfinder.pet.animal['$t']);
+            console.log(petfinder.pet.sex['$t']);
+            console.log(petfinder.pet.age['$t']);
+            console.log(petfinder.pet.description['$t']);
+            console.log(petfinder.pet.id['$t']);
+
+
+            var infoHTML = '<ul>';
+            infoHTML += '<li>';
+            infoHTML += '<strong>Description</strong><br>';
+            infoHTML += petfinder.pet.description['$t'];
+            infoHTML += petfinder.pet.animal['$t'];
+            infoHTML += '</li>';
+
+            infoHTML += '</li>';
+
+            infoHTML += '</ul>';
+            // return infoHTML;
+            $('#petfinderInfo').html(infoHTML);
+
+            // $('#petfinderInfo').html(petfinder.pet.description['$t']);
+            //
+            console.log(petfinder);
+        },
+        error : function(request,error)
+        {
+            alert("Request: "+JSON.stringify(request));
         }
-      }
-    });
-  };
-
-  getNextDog();
-
-  $('.hot-button').click(function(event){
-    var liked = $(event.currentTarget).hasClass('hot-button');
-
-
-$.ajax({
-      url: '/pets/' + dogId,
-      method: 'PUT',
-      dataType: 'json',
-      data: {liked: liked},
-      success: function(dogData) {
-        getNextDog();
-        $('#liked-dogs').append('<div class="liked-dog-name"><img src="' + dogData.photo + '"/>' + "<br> " + dogData.name + "<br> " + dogData.age + ' years </div><br>' );
-      }
-    });
-  });
-
-  $('.not-hot-button').click(function(event){
-    var notliked = $(event.currentTarget).hasClass('not-hot-button');
-
-
-$.ajax({
-      url: '/pets/' + dogId,
-      method: 'PUT',
-      dataType: 'json',
-      data: {liked: notliked},
-      success: function(dogData) {
-        getNextDog();
-      }
-    });
-  });
-  $('#exit').click(function(event){
-    navigateToContentSection($('#main'));
-  });
-$('#display-liked').click(function(event){
-    navigateToContentSection($('#liked'));
-  });
+      });
+     });
 });
 
-var contentSectionList = [$('#main'), $('#liked')];
 
-function navigateToContentSection(sectionToDisplay){
-  $.each(contentSectionList, function(i, contentSection){
-    if(contentSection.css('display') != 'none'){
-      contentSection.hide('slow', function(){
-        sectionToDisplay.show('slow');
-      });
-    }
-  });
-}
+
+
+
+
---
+++ public/javascripts/script1.js
@@ -0,0 +1,75 @@
+$(document).ready(function(){
+  var dogId;
+
+  function getNextDog() {
+    $.ajax({
+      url: '/pets',
+      method: 'GET',
+      dataType: 'json',
+      success: function(dogData) {
+        if (dogData.status) {
+          $('.dog').html('');
+          alert(dogData.status);
+        } else {
+          dogId = dogData._id;
+
+          $('.dog').html(
+            '<img src="' + dogData.photo + '"/>' +
+            '<div class="dog-name">' + dogData.name + "<br> " + dogData.age + ' years </div>'
+          );
+        }
+      }
+    });
+  };
+
+  getNextDog();
+
+  $('.hot-button').click(function(event){
+    var liked = $(event.currentTarget).hasClass('hot-button');
+
+
+$.ajax({
+      url: '/pets/' + dogId,
+      method: 'PUT',
+      dataType: 'json',
+      data: {liked: liked},
+      success: function(dogData) {
+        getNextDog();
+        $('#liked-dogs').append('<div class="liked-dog-name"><img src="' + dogData.photo + '"/>' + "<br> " + dogData.name + "<br> " + dogData.age + ' years </div><br>' );
+      }
+    });
+  });
+
+  $('.not-hot-button').click(function(event){
+    var notliked = $(event.currentTarget).hasClass('not-hot-button');
+
+
+$.ajax({
+      url: '/pets/' + dogId,
+      method: 'PUT',
+      dataType: 'json',
+      data: {liked: notliked},
+      success: function(dogData) {
+        getNextDog();
+      }
+    });
+  });
+  $('#exit').click(function(event){
+    navigateToContentSection($('#main'));
+  });
+$('#display-liked').click(function(event){
+    navigateToContentSection($('#liked'));
+  });
+});
+
+var contentSectionList = [$('#main'), $('#liked')];
+
+function navigateToContentSection(sectionToDisplay){
+  $.each(contentSectionList, function(i, contentSection){
+    if(contentSection.css('display') != 'none'){
+      contentSection.hide('slow', function(){
+        sectionToDisplay.show('slow');
+      });
+    }
+  });
+}
